88286568743ec198ad639b3eed136180ab5f068a
Matt Smallhouse <smallhou@bc.edu>
2023-05-20 16:53:00 -0400

    Calculator fully functional now - Added "key" class to all keys in html - Wrote recievePress() in js to register each button press correctly, doing different things for numbers, symbols, or clear button - Booleans firstNumberEntered and secondNumberEntered added for calculator to function properly - updateScreen() added to show the correct value during and after calculations

--- calculate.js
+++ calculate.js
@@ -16,6 +16,8 @@ function divide (a, b) {
 }
 
 function operate(operator, a, b) {
+    a = parseInt(a);
+    b = parseInt(b);
     switch (operator) {
         case '+':
             return add(a, b);
@@ -28,6 +30,72 @@ function operate(operator, a, b) {
     }
 }
 
+function pressedEquals() {
+    firstNumber = operate(operator, firstNumber, secondNumber);
+    updateScreen(firstNumber);
+    secondNumber = null;
+    firstNumberEntered = false;
+}
+
+function pressedOperator(valueEntered) {
+    if (firstNumberEntered) {
+        firstNumber = operate(operator, firstNumber, secondNumber);
+        secondNumber = null;
+    }
+    firstNumberEntered = true;
+    operator = valueEntered;
+    updateScreen(valueEntered);
+}
+
+function clearScreen() {
+    updateScreen('0');
+    firstNumber = null;
+    secondNumber = null;
+    firstNumberEntered = false;
+    secondNumberEntered = false;
+}
+
+function recievePress (event) {
+    valueEntered = event.textContent;
+    if (valueEntered === '=') pressedEquals();
+    else if (valueEntered === 'clear') clearScreen();
+    else if ('/*-+'.includes(valueEntered)) pressedOperator(valueEntered);
+    else if (!firstNumberEntered) {
+        updateFirstNumber(valueEntered);
+    }
+    else {
+        updateSecondNumber(valueEntered);
+    }
+}
+
+function updateFirstNumber(value) {
+    if (!firstNumber) firstNumber = value;
+    else firstNumber += value;
+    updateScreen(firstNumber);
+}
+
+function updateSecondNumber(value) {
+    if (!secondNumber) secondNumber = value;
+    else secondNumber += value;
+    updateScreen(secondNumber);
+}
+
+function updateScreen(value) {
+    console.log(screen)
+    screen.textContent = '';
+    screen.textContent = value;
+}
+
 let firstNumber;
 let secondNumber;
+let answer;
 let operator;
+let firstNumberEntered = false;
+let secondNumberEntered = false;
+
+const screen = document.querySelector('.screen div')
+const keys = document.querySelectorAll('.key');
+const keysArray = Array.from(keys);
+keysArray.forEach(element => {
+    element.addEventListener('click', () => recievePress(element));
+});
