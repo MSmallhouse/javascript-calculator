5a1ceb2807723d7cd21ae24da3f2210d2e155b5d
Matt Smallhouse <smallhou@bc.edu>
2023-05-20 14:18:38 -0400

    Added functions for calculations and wrapper operate() to call the correct calculation function

---
+++ calculate.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    // Math.round returns nearest whole number, so do this to round to 1 decimal
+    return Math.round((a/b) * 10)/10;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
